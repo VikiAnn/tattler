7c6de6b578d855483047090a7c3680131c7af6c3
Victoria Harrod <victoria.harrod@gmail.com>
2014-04-29 21:35:53 -0400

    Hooked up the completed checkbox
    Removed checkbox from index page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140425135027) do
+ActiveRecord::Schema.define(version: 20140430005847) do
 
   create_table "tasks", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20140425135027) do
     t.date     "deadline"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "completed"
   end
 
 end
